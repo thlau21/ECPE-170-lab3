1719c10f64ad26c5e4c36f88cec8ae08de2b410c
Thomas Lau <t_lau11@u.pacific.edu>
2022-02-09 16:31:41 -0800

    testing

---
+++ functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+
+
+char** createArray(int size);
+void printBoard(char** arr, int size);
+void fillArray(char** myArray, int rows, int cols);
+void deleteArray(int** arr, int rows, int cols);
+
+#endif
